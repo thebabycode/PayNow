9d4db2c30caba6b961547ee470ae041a7097850e
Johns Baby <johnsbabymemana@gmail.com>
2022-10-25 11:22:11 +0530

    Scanner Working and Code showing as textview

--- app/src/main/java/com/example/paynow/MainActivity.java
+++ app/src/main/java/com/example/paynow/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
@@ -13,43 +14,48 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
-    private CodeScanner mCodeScanner;
+    CodeScanner codeScanner;
+    CodeScannerView scannView ;
+    TextView resultData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+        scannView = findViewById(R.id.scannerview);
+        codeScanner = new CodeScanner(this, scannView);
+        resultData = findViewById(R.id.tv_textview);
+
+        codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull final Result result) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        resultData.setText(result.getText());
                     }
                 });
             }
         });
-        scannerView.setOnClickListener(new View.OnClickListener() {
+        scannView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mCodeScanner.startPreview();
+                codeScanner.startPreview();
             }
         });
+    }
+
+    @Override
+    protected void onPause() {
+        codeScanner.releaseResources();
+        super.onPause();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        mCodeScanner.startPreview();
-    }
-
-    @Override
-    protected void onPause() {
-        mCodeScanner.releaseResources();
-        super.onPause();
+        codeScanner.startPreview();
     }
 }
